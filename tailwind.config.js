926b10cd545490cf4e875d163686adc999b409f9
Setthasiri Ngamsom <ngamsomset@setthasiris-mbp.home>
2021-03-24 20:19:11 +1100

    -initial-

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#323ca8',
+        secondary: {
+          100: '#32a852',
+          200: '#2b9447',
+        },
+      },
+      fontFamily: {
+        mainFont: ['Montserrat'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
